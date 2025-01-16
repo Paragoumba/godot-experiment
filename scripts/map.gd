59e25199040d21777069f09c3cfd2108f9e0ca7b
Paragoumba <22895992+Paragoumba@users.noreply.github.com>
2025-01-16 15:49:22 +0100

    Improved the map

--- scripts/map.gd
+++ scripts/map.gd
@@ -1,7 +1,7 @@
 extends TextureRect
 
 func _ready():
-	get_node("/root/Root/World").connect("world_generated", handle_world_generated)
+	$/root/Root/World/Chunk.world_generated.connect(handle_world_generated)
 
 func handle_world_generated(image: Image):
 	texture = ImageTexture.create_from_image(image)
