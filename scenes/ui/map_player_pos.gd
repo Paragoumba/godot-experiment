760f0e72c5275b6c65c70010e9f32f349b2208eb
Paragoumba <22895992+Paragoumba@users.noreply.github.com>
2025-01-16 15:49:22 +0100

    Improved the map

---
+++ scenes/ui/map_player_pos.gd
@@ -0,0 +1,17 @@
+extends CenterContainer
+
+func _process(_delta):
+	var player = $/root/Root/Player
+	var playerPos = player.position
+	var world = $/root/Root/World
+	var worldPos = world.position
+	var playerMarker = $Player
+	var worldMeshSize = $/root/Root/Platform/PlatformMesh.mesh.size
+	var sizeInside = size - playerMarker.size
+	
+	playerMarker.pivot_offset = playerMarker.size / 2
+	
+	playerMarker.position = size / 2 + (Vector2(playerPos.x, playerPos.z) - Vector2(worldPos.x, worldPos.z)) * sizeInside / Vector2(worldMeshSize.x, worldMeshSize.z)
+	playerMarker.position.x = max(0, min(sizeInside.x, playerMarker.position.x))
+	playerMarker.position.y = max(0, min(sizeInside.y, playerMarker.position.y))
+	playerMarker.rotation = -player.rotation.y
