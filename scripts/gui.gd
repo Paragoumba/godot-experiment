77a3c8a23b69752049c7b5a627960459a7aa9350
Paragoumba <22895992+Paragoumba@users.noreply.github.com>
2025-01-16 15:53:37 +0100

    Added counter when putting the ball inside the basket

--- scripts/gui.gd
+++ scripts/gui.gd
@@ -1,4 +1,11 @@
 extends MarginContainer
 
+var counter = 0
+
 func _on_node_3d_debug_toggled():
 	$Debug.visible = get_parent().debug
+
+func _on_basket_ball_entered_basket():
+	counter += 1
+	print(counter)
+	$Counter.text = "Counter: " + str(counter)
