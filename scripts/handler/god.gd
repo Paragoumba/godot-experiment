c8d86f8f16a1bb4e969d381b6adbb16976dcee0a
Paragoumba <22895992+Paragoumba@users.noreply.github.com>
2025-01-16 15:55:35 +0100

    Added possibility to run (gotta go fast)

--- scripts/handler/god.gd
+++ scripts/handler/god.gd
@@ -4,6 +4,8 @@ const MOUSE_SENSIVITY = 0.3
 const SPEED = 10.0
 const JUMP_VELOCITY = 4.5
 
+var running: bool = false
+
 var player: Node
 var camera: Node
 var inventory: Node
@@ -21,33 +23,31 @@ func _physics_process(delta):
 		position.y += JUMP_VELOCITY * delta
 	elif Input.is_action_pressed("crouch"):
 		position.y -= JUMP_VELOCITY * delta
-		
+
 	var input_dir = Input.get_vector("leftward", "rightward", "forward", "backward").rotated(-rotation.y)
-	
+
+	#if input_dir.length() > 0:
+	#	$MovePointVis.position = $MovePoint.position + Vector3(input_dir.x, 0, input_dir.y)
+
 	if input_dir:
-		var x = round(input_dir.x)
-		var z = round(input_dir.y)
-		
-		position.x += x * SPEED * delta
-		position.z += z * SPEED * delta
+		var trueSpeed = SPEED * 2 if running else SPEED
+		input_dir *= trueSpeed
+		position.x += input_dir.x * delta
+		position.z += input_dir.y * delta
 
 func _input(event):
-	
-	#camera = $/root/Root/Player/FirstPersonCamera
-	#player = $/root/Root/Player
-	
 	if !$/root/Root/UI/Inventory.visible:
 		if event is InputEventMouseMotion:
 			var angleX = deg_to_rad(-event.relative.y * MOUSE_SENSIVITY)
 			var angleY = deg_to_rad(-event.relative.x * MOUSE_SENSIVITY)
-			
+
 			if camera.rotation_degrees.x + angleX > 90:
 				camera.rotation_degrees.x = 90
 			elif camera.rotation_degrees.x + angleX < -90:
 				camera.rotation_degrees.x = -90
 			else:
 				camera.rotate_object_local(Vector3.RIGHT, angleX)
-				
+
 			player.rotate_y(angleY)
-			
+
 		#elif event is InputEventJoypadMotion
