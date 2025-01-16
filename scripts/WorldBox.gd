5c768f84c3068c48fea0164e478429d53c949780
Paragoumba <22895992+Paragoumba@users.noreply.github.com>
2025-01-16 16:00:20 +0100

    Added a group to the player when in god mode

--- scripts/WorldBox.gd
+++ scripts/WorldBox.gd
@@ -1,6 +1,9 @@
 extends Area3D
 
 func _on_body_exited(body):
+	if body.is_in_group("god"):
+		pass
+
 	print(body.name + " has exited the world")
 	body.position = get_parent().get_node("Platform/Spawn").position
 	body.rotation = Vector3.ZERO
