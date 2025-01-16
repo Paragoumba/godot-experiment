160fa2c78836602d624f834be8e615ca90f2ce07
Paragoumba <22895992+Paragoumba@users.noreply.github.com>
2025-01-16 15:55:35 +0100

    Added possibility to run (gotta go fast)

--- scripts/handler/player.gd
+++ scripts/handler/player.gd
@@ -6,6 +6,8 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const RAY_LENGTH = 5
 
+var running: bool = true
+
 var inventory: Node
 var player: Node
 var camera: Node
@@ -17,14 +19,14 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 func processCameraRotation(relative: Vector2):
 	var angleX = deg_to_rad(-relative.x)
 	var angleY = deg_to_rad(-relative.y)
-	
+
 	if camera.rotation_degrees.x + angleX > 90:
 		camera.rotation_degrees.x = 90
 	elif camera.rotation_degrees.x + angleX < -90:
 		camera.rotation_degrees.x = -90
 	else:
 		camera.rotate_object_local(Vector3.RIGHT, angleX)
-		
+
 	player.rotate_y(angleY)
 
 func _ready():
@@ -37,8 +39,6 @@ func load_nodes():
 	animationPlayer = $'character-human'/AnimationPlayer
 
 func _physics_process(delta):
-	#print(velocity, is_on_floor())
-
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -48,18 +48,13 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	var input_dir = Input.get_vector("leftward", "rightward", "forward", "backward")
+
+	var trueSpeed = SPEED * 2 if running else SPEED
 	
-	if input_dir.length() > 0:
-		$MovePointVis.position = $MovePoint.position + Vector3(input_dir.x, 0, input_dir.y)
+	input_dir = input_dir.rotated(-rotation.y) * trueSpeed
 
-	input_dir = input_dir.rotated(-rotation.y)
-	#print(input_dir.length())
-	#$MovePointVis.position = $MovePoint.position + Vector3(input_dir.length(), 0, input_dir.length())
-
-	#print(input_dir)
-
-	velocity.x = input_dir.x * SPEED
-	velocity.z = input_dir.y * SPEED
+	velocity.x = input_dir.x
+	velocity.z = input_dir.y
 
 	move_and_slide()
 	
@@ -67,19 +62,19 @@ func _input(event):
 	if !inventory.visible:
 		var relative = Vector2()
 		var cameraMoved = false
-		
+
 		if event is InputEventMouseMotion:
 			cameraMoved = true
 			relative.x = event.relative.y * MOUSE_SENSIVITY
 			relative.y = event.relative.x * MOUSE_SENSIVITY
-			
+
 		elif event is InputEventJoypadMotion:
 			cameraMoved = true
 			relative = Input.get_vector("look_up", "look_down", "look_left", "look_right") * CONTROLLER_SENSIVITY
-		
+
 		if cameraMoved:
 			processCameraRotation(relative)
-		
+
 	if event.is_action_pressed("kick"):
 			animationPlayer.play("attack-melee-left")
 			Input.start_joy_vibration(0, 1, 0, 0.3)
@@ -97,6 +92,6 @@ func _input(event):
 				var object: Node = result.collider
 				if object.is_in_group("enemy"):
 					object.hit()
-				
+
 				elif "linear_velocity" in object:
 					object.linear_velocity += (object.position - origin).normalized() * RAY_LENGTH * 5
