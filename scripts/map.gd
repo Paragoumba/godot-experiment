3c6432934a22209f089bc6b6a64ae6f7bc074ca9
Paragoumba <22895992+Paragoumba@users.noreply.github.com>
2025-03-01 00:46:58 +0100

    Updated chunk management

--- scripts/map.gd
+++ scripts/map.gd
@@ -1,7 +1,7 @@
 extends TextureRect
 
 func _ready():
-	$/root/Root/World/Chunk.world_generated.connect(handle_world_generated)
+	$/root/Root/World.world_generated.connect(handle_world_generated)
 
 func handle_world_generated(image: Image):
 	texture = ImageTexture.create_from_image(image)
